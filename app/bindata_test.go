6e1dcaeec54b32714991d76f114dd7c8dc9ca95e
Atsushi Nagase <a@ngs.io>
2018-02-17 11:15:18 +0900

    Add bindata test

---
+++ app/bindata_test.go
@@ -0,0 +1,55 @@
+package app
+
+import (
+	"sort"
+	"testing"
+)
+
+func TestBindataRead(t *testing.T) {
+	b, err := bindataRead([]byte(""), "foo")
+	for _, test := range []Test{
+		{0, len(b)},
+		{`Read "foo": EOF`, err.Error()},
+	} {
+		test.Compare(t)
+	}
+}
+
+func TestAssetDir(t *testing.T) {
+	files, err := AssetDir("assets")
+	sort.Strings(files)
+	Test{[]string{
+		"favicon.ico",
+		"index.html",
+		"success.html",
+	}, files}.DeepEqual(t)
+	Test{true, err == nil}.Compare(t)
+}
+
+func TestAssetNames(t *testing.T) {
+	names := AssetNames()
+	sort.Strings(names)
+	Test{[]string{
+		"assets/favicon.ico",
+		"assets/index.html",
+		"assets/success.html",
+	}, names}.DeepEqual(t)
+}
+
+func TestAssetInfo(t *testing.T) {
+	info, err := AssetInfo("assets/index.html")
+	info2, err2 := AssetInfo("assets/index2.html")
+	for _, test := range []Test{
+		{true, err == nil},
+		{"assets/index.html", info.Name()},
+		{int64(719), info.Size()},
+		{"-rw-r--r--", info.Mode().String()},
+		{false, info.ModTime().IsZero()},
+		{false, info.IsDir()},
+		{true, info.Sys() == nil},
+		{"AssetInfo assets/index2.html not found", err2.Error()},
+		{true, info2 == nil},
+	} {
+		test.Compare(t)
+	}
+}
